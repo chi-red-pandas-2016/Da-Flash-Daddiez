fdab12f4d388d1787f70012679de6ed24bec0ac5
Nick Block <nicholasblock78@gmail.com>
2016-08-05 15:53:37 -0500

    add 3 test questions

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Deck.create(name: "Celebrities")
+
+Card.create(deck_id: 1, question: "What was Whitney Houston's drug of choice?", answer: "Crack")
+Card.create(deck_id: 1, question: "Nicholas Cage once had a pet ____", answer: "Octopus")
+Card.create(deck_id: 1, question: "Bill Murray was arrested for?", answer: "Smuggling dope")

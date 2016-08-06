364f20a359d284f6a7cc93bf20fe8ec05e6e6f55
tnaughts <tnaughts@gmail.com>
2016-08-06 11:19:25 -0500

    update home page logic on layout with user auth

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -5,9 +5,11 @@ end
 post '/login' do
   user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
+      @login_error = nil
       session[:user_id] = user.id
       redirect "/users/#{user.id}" #need to send back to index/flashcardpage
     else
+      @login_error = "Username or Password incorrect"
       erb :login
     end
 end

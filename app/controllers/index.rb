12bbe7c64f2a921d525b6ed61087b5e59dc2e830
Nick Block <nicholasblock78@gmail.com>
2016-08-06 22:13:49 -0500

    rename home.erb to profile.erb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,6 +6,8 @@ get '/users/:user_id' do
 
   redirect '/login' unless session[:user_id]
   @current_user = User.find(params[:user_id])
-  erb :home
+  erb :profile
 end
 
+
+#We need to make it so that once a user has logging in they will see the 

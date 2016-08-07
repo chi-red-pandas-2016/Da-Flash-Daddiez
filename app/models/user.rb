5eaac2e2fb95a392b347020516b6c710ba47a35f
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-06 23:10:27 -0500

    Refactor to make more restful

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
  has_many :rounds
  has_many :guesses, through: :rounds
   validates :username, uniqueness: true, presence: true
+
   def password
     @password ||= BCrypt::Password.new(hashed_password)
   end
@@ -16,5 +17,3 @@ class User < ActiveRecord::Base
   end
 
 end
-
-User.first.guesses

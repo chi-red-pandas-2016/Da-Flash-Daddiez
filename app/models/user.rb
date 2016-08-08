cb1cbc9959cc4b4a4f96e664bf9d89b765655c95
dmanliclic715 <dmanliclic715@gmail.com>
2016-08-07 21:49:24 -0500

    Complete Login,Logout,Register schtuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,30 @@ class User < ActiveRecord::Base
  has_many :guesses, through: :rounds
   validates :username, uniqueness: true, presence: true
   validates :hashed_password, presence: true
+  validate :check_pass
+
+
 
   def password
     @password ||= BCrypt::Password.new(hashed_password)
   end
 
   def password=(plaintext_password)
+    @plaintext = plaintext_password
     @password = BCrypt::Password.create(plaintext_password)
     self.hashed_password = @password
   end
 
+
+
   def authenticate(user_entered_password)
     self.password == user_entered_password
+  end
+
+  def check_pass
+    if !(@plaintext.length > 1)
+      errors.add(:hashed_password, "Password must be longer.")
+    end
   end
 
 end
